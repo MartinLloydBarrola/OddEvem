6dbb70e1c07d7e14bee252fbc9e710f8182c2112
MartinLloydBarrola <martinbarrola@gmail.com>
2024-06-08 07:43:08 +0000

    OddEven

---
+++ OddEven.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class OddEven {
+
+    public static void main(String[] args) {
+       
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int input = scanner.nextInt();
+        
+        if(input == 0){
+            System.out.println("The number is zero");
+        }
+        else if(input % 2 == 0){
+            System.out.println("Even number");
+        }else {
+            System.out.println("odd number");
+        }
+
+        if(input > 0){
+            System.out.println("Positive number");
+        }else {
+            System.out.println("Negative number");
+        }
+
+    }
+}
